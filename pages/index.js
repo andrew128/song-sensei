2137cef6ed8c6e422ec07e7dfff32c15b91e422b
Andrew Wei <andrewwei98@gmail.com>
2023-07-01 18:22:36 -0700

    use langchain api instead of OpenAI Api directly

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,27 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
+import { BufferWindowMemory } from "langchain/memory";
+import { ConversationChain } from "langchain/chains";
+import { OpenAI } from "langchain/llms/openai";
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
   const [result, setResult] = useState();
-  const [conversationHistory, setConversationHistory] = useState([]);
 
-  const updateConversationHistoryValue = (newConversationHistory) => {
-    if (conversationHistory.length > 10) {
-      newConversationHistory.shift()
-    }
-    setConversationHistory(newConversationHistory);
-  }
+  const model = new OpenAI({openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, temperature: 0.8});
+  const memory = new BufferWindowMemory({ k: 100 });
+  const chain = new ConversationChain({ llm: model, memory: memory });
 
   async function onSubmit(event) {
     event.preventDefault();
     try {
-      let newConversationHistory = [...conversationHistory, userInput];
-      updateConversationHistoryValue(newConversationHistory);
+      const res = await chain.call({ input: userInput });
+      console.log({ res });
 
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ musicRequest: newConversationHistory.join('.') }),
-      });
-
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-
-      setResult(data.result);
+      setResult(res.response);
       setUserInput("");
     } catch(error) {
-      // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
