8abb3eff3c7fd3c18c4ad53b244c8783828c6705
Andrew Wei <andrewwei98@gmail.com>
2023-07-04 16:35:02 -0700

    remove extra unused handleLogin code

--- pages/components/SpotifyAuth.js
+++ pages/components/SpotifyAuth.js
@@ -32,7 +32,7 @@ async function generateCodeChallenge(codeVerifier) {
   return base64encode(digest);
 }
 
-const SpotifyAuth = ({ onLogin }) => {
+const SpotifyAuth = () => {
   async function performPKCEAuthorization() {
     // Create a code verifier
     const codeVerifier = generateRandomString(128);
@@ -68,82 +68,33 @@ const SpotifyAuth = ({ onLogin }) => {
     const codeVerifier = generateRandomString(128);
 
     // Hash the codeVerifier to create a code challenge.
-    await generateCodeChallenge(codeVerifier).then((codeChallenge) => {
-      // Send the code challenge in the user auth request.
-      let state = generateRandomString(16);
+    await generateCodeChallenge(codeVerifier)
+      .then((codeChallenge) => {
+        // Send the code challenge in the user auth request.
+        let state = generateRandomString(16);
 
-      localStorage.setItem("code_verifier", codeVerifier);
+        localStorage.setItem("code_verifier", codeVerifier);
 
-      let args = new URLSearchParams({
-        response_type: "code",
-        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
-        scope: scope,
-        redirect_uri: redirectUri,
-        state: state,
-        code_challenge_method: "S256",
-        code_challenge: codeChallenge,
+        let args = new URLSearchParams({
+          response_type: "code",
+          client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+          scope: scope,
+          redirect_uri: redirectUri,
+          state: state,
+          code_challenge_method: "S256",
+          code_challenge: codeChallenge,
+        });
+
+        window.location.href = `${authorizationEndpoint}?${args}`;
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
       });
-
-      window.location.href = `${authorizationEndpoint}?${args}`;
-    });
   };
-
-  // React.useEffect(() => {
-  //   const getToken = async (code, verifier) => {
-  //     const data = {
-  //       grant_type: "authorization_code",
-  //       code,
-  //       redirect_uri: redirectUri,
-  //       code_verifier: verifier,
-  //       client_id: process.env.CLIENT_ID,
-  //     };
-
-  //     try {
-  //       const response = await axios.post(
-  //         tokenEndpoint,
-  //         queryString.stringify(data),
-  //         {
-  //           headers: {
-  //             "Content-Type": "application/x-www-form-urlencoded",
-  //           },
-  //         }
-  //       );
-
-  //       const { access_token, expires_in, refresh_token } = response.data;
-
-  //       if (access_token) {
-  //         onLogin(access_token, expires_in, refresh_token);
-  //       }
-  //     } catch (error) {
-  //       console.error(
-  //         "Failed to exchange authorization code for access token:",
-  //         error
-  //       );
-  //     }
-  //   };
-
-  //   const { code, error } = queryString.parse(window.location.search);
-
-  //   if (code) {
-  //     getToken(code);
-  //   } else if (error) {
-  //     console.error("Authorization error:", error);
-  //   }
-  // }, [onLogin]);
-
-//   async function handleLogin() {
-//     const urlParams = new URLSearchParams(window.location.search);
-//     let code = urlParams.get("code");
-
-//     console.log("UrlParams: ", urlParams);
-//     console.log("code: ", code);
-//   }
 
   return (
     <div>
-      <button onClick={handleClick}>
-        Login with Spotify
-      </button>
+      <button onClick={handleClick}>Login with Spotify</button>
     </div>
   );
 };
