7b08161798e6427ed2582f037c503acf7f29266b
Andrew Wei <andrewwei98@gmail.com>
2023-07-04 18:29:20 -0700

    refactored spotify auth request and llm chat into separate pages

---
+++ pages/chat/index.js
@@ -0,0 +1,63 @@
+import Head from "next/head";
+import { useState } from "react";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+import styles from "./../index.module.css";
+
+const SubmitRequest = () => {
+  const [userInput, setUserInput] = useState("");
+  const [result, setResult] = useState();
+
+  const chat = new ChatOpenAI({
+    openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    temperature: 0,
+  });
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await chat.call([
+        new SystemChatMessage(
+          "You are a helpful assistant that comes up with a playlist of songs." +
+            "You will give the playlist of songs in a numbered list." +
+            "If you can't interpret a message as a song request you will truthfully say you do not know."
+        ),
+        new HumanChatMessage(userInput),
+      ]);
+      console.log({ res });
+
+      setResult(res.text);
+      setUserInput("");
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>🎵 Song Sensei 🎵</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h3>🎵 Song Sensei 🎵</h3>
+        <div>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="musicRequest"
+              placeholder="Ask for music"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+            />
+            <input type="submit" value="🗣" />
+          </form>
+          <div className={styles.result}>{result}</div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default SubmitRequest;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,69 @@
 import Head from "next/head";
-import SpotifyAuth from "./components/SpotifyAuth";
-import SubmitRequest from "./components/SubmitRequest";
 import styles from "./index.module.css";
+import React from "react";
+
+const redirectUri = "http://localhost:3000/chat";
+const scope = "playlist-modify-public";
+const authorizationEndpoint = "https://accounts.spotify.com/authorize";
+
+function generateRandomString(length) {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+  function base64encode(string) {
+    return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=+$/, "");
+  }
+
+  const encoder = new TextEncoder();
+  const data = encoder.encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
+
+  return base64encode(digest);
+}
 
 export default function Home() {
+  const handleClick = async () => {
+    // Create a code verifier
+    const codeVerifier = generateRandomString(128);
+
+    // Hash the codeVerifier to create a code challenge.
+    await generateCodeChallenge(codeVerifier)
+      .then((codeChallenge) => {
+        // Send the code challenge in the user auth request.
+        let state = generateRandomString(16);
+
+        localStorage.setItem("code_verifier", codeVerifier);
+
+        let args = new URLSearchParams({
+          response_type: "code",
+          client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+          scope: scope,
+          redirect_uri: redirectUri,
+          state: state,
+          code_challenge_method: "S256",
+          code_challenge: codeChallenge,
+        });
+
+        console.log("args", args);
+
+        window.location.href = `${authorizationEndpoint}?${args}`;
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
+  };
+
   return (
     <div>
       <Head>
@@ -13,14 +73,7 @@ export default function Home() {
       <main className={styles.main}>
         <h3>🎵 Song Sensei 🎵</h3>
 
-        <div className="App">
-          <SpotifyAuth />
-        </div>
-
-
-        <div className="App">
-          <SubmitRequest />
-        </div>
+        <button onClick={handleClick}>Login with Spotify</button>
       </main>
     </div>
   );
