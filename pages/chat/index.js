eda4e55656f87b952de614dd4768304cf0b331d2
Andrew Wei <andrewwei98@gmail.com>
2023-07-04 22:19:30 -0700

    add code to get authentication token

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -2,7 +2,16 @@ import Head from "next/head";
 import { useState } from "react";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+import React from "react";
 import styles from "./../index.module.css";
+
+const redirectUri = "http://localhost:3000/chat";
+
+function getResponseCode() {
+  const urlParams = new URLSearchParams(window.location.search);
+  console.log(urlParams.get("code"));
+  return urlParams.get("code");
+}
 
 const SubmitRequest = () => {
   const [userInput, setUserInput] = useState("");
@@ -13,9 +22,45 @@ const SubmitRequest = () => {
     temperature: 0,
   });
 
+  React.useEffect(() => {
+    console.log("inside of React.useEffect", getResponseCode());
+
+    let codeVerifier = localStorage.getItem("code_verifier");
+
+    let body = new URLSearchParams({
+      grant_type: "authorization_code",
+      code: getResponseCode(),
+      redirect_uri: redirectUri,
+      client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+      code_verifier: codeVerifier,
+    });
+
+    const response = fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: body,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("HTTP status " + response.status);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data.access_token", data.access_token);
+        localStorage.setItem("access_token", data.access_token);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   async function onSubmit(event) {
     event.preventDefault();
     try {
+      const code = getResponseCode();
       const res = await chat.call([
         new SystemChatMessage(
           "You are a helpful assistant that comes up with a playlist of songs." +
@@ -42,19 +87,17 @@ const SubmitRequest = () => {
 
       <main className={styles.main}>
         <h3>🎵 Song Sensei 🎵</h3>
-        <div>
-          <form onSubmit={onSubmit}>
-            <input
-              type="text"
-              name="musicRequest"
-              placeholder="Ask for music"
-              value={userInput}
-              onChange={(e) => setUserInput(e.target.value)}
-            />
-            <input type="submit" value="🗣" />
-          </form>
-          <div className={styles.result}>{result}</div>
-        </div>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="musicRequest"
+            placeholder="Ask for music"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <input type="submit" value="🗣" />
+        </form>
+        <div className={styles.result}>{result}</div>
       </main>
     </div>
   );
